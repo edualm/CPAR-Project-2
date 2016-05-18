fce031ec1eed497eaf9d523f945d51d7cbc7f05d
Silmarie <lyra.belacqua4@gmail.com>
2016-05-18 12:07:39 +0100

    MPI

--- SieveEratosthenes/main.cpp
+++ SieveEratosthenes/main.cpp
@@ -11,6 +11,7 @@
 #include <cmath>
 #include <ctime>
 #include <cstring>
+#include "mpi.h"
 
 void printUnmarked(long long upperBound, bool *marked) {
     for (long long i = 0; i <= upperBound; i++)
@@ -19,24 +20,33 @@ void printUnmarked(long long upperBound, bool *marked) {
 }
 
 void runEratosthenesSieve(long long upperBound, int numThreads, bool print) {
-    int upperBoundSquareRoot = (int)sqrt(upperBound);
-    bool *marked = new bool[upperBound + 1];
+    int upperBoundSquareRoot = (int)sqrt(upperBound), size = upperBound + 1, rank, numtasks;
+    bool *marked = new bool[size];
+    bool *recvbuf = new bool[size/4];
+
+     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     
-    memset(marked, 0, sizeof(bool) * (upperBound + 1));
-    
-    for (long long m = 2; m <= upperBoundSquareRoot; m++)
+    memset(marked, 0, sizeof(bool) * (size));
+
+    for (long long m = 2; m <= upperBoundSquareRoot; m++) {
+        MPI_Bcast(&m, 1, MPI_LONG_LONG, 0, MPI_COMM_WORLD);
         if (!marked[m])
-#pragma omp parallel for num_threads(numThreads)
-            for (long long k = m * m; k <= upperBound; k += m)
-                marked[k] = true;
-    
+        MPI_Scatter(marked, size, MPI_C_BOOL, recvbuf, size/4, MPI_C_BOOL, 0, MPI_COMM_WORLD);
+            for (long long k = (rank*upperBound/4 + 2) * m; k <= upperBound/4*(rank + 1); k += m)
+                recvbuf[k+(rank*size/4)] = true;
+                //marked[k] = true;
+    }
+    MPI_Gather(&recvbuf, size/4, MPI_C_BOOL, marked, size, MPI_C_BOOL, 0, MPI_COMM_WORLD);
+
     if (print)
         printUnmarked(upperBound, marked);
     
     delete [] marked;
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char * argv[]) {
+MPI_Init(&argc,&argv);
     // insert code here...
     runEratosthenesSieve(pow(2, 30), 4, false);
     std::cout << "Hello, World!\n";
