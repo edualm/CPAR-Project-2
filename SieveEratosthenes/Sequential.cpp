94e919be0ad3d4fbef1fc85d1c8fead94620394d
Eduardo Almeida <me@edr.io>
2016-05-22 19:33:36 +0100

    Updated source.

--- SieveEratosthenes/Sequential.cpp
+++ SieveEratosthenes/Sequential.cpp
@@ -1,6 +1,6 @@
 //
 //  SieveEratosthenes
-//  OpenMP Version
+//  Sequential Version
 //
 //  Based on http://www.algolist.net/Algorithms/Number_theoretic/Sieve_of_Eratosthenes
 //
@@ -11,43 +11,41 @@
 #include <ctime>
 #include <cstring>
 
-#include <omp.h>
+#include <omp.h>    //  Only for omp_get_wtime()
 
 void printUnmarked(long long upperBound, bool *marked) {
     for (long long i = 0; i <= upperBound; i++)
-        if (!marked[i])
-            std::cout << i << " ";
+            if (!marked[i])
+                std::cout << i << " ";
 }
 
-void runEratosthenesSieve(long long upperBound, int numThreads, bool print) {
+void runEratosthenesSieve(long long upperBound, bool print) {
     double time1, time2;
     
     int upperBoundSquareRoot = (int)sqrt(upperBound);
     bool *marked = new bool[upperBound + 1];
-    
+     
     memset(marked, 0, sizeof(bool) * (upperBound + 1));
     
     time1 = omp_get_wtime();
-    
+     
     for (long long m = 2; m <= upperBoundSquareRoot && !marked[m]; m++)
-#pragma omp parallel for num_threads(numThreads)
         for (long long k = m * m; k <= upperBound; k += m)
             marked[k] = true;
     
     time2 = omp_get_wtime();
-    
+     
     if (print)
         printUnmarked(upperBound, marked);
     
-    std::cout << "Upper bound: " << upperBound << " - Time: " << time2 - time1 << " - Threads: " << numThreads << std::endl;
-    
+    std::cout << "Upper bound: " << upperBound << " - Time: " << time2 - time1 << std::endl;
+     
     delete [] marked;
 }
 
 int main(int argc, const char * argv[]) {
     for (int i = 25; i <= 32; i++)
-        for (int j = 1; j <= 4; j++)
-            runEratosthenesSieve(pow(2, i), j, false);
-    
-    return 0;
+        runEratosthenesSieve(pow(2, i), false);
+
+     return 0;
 }
